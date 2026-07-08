209c15e3cdcca495757e0abc5d9f2c5661f1612e
Kekgaugetswe <Kekgaugetswe@gmail.com>
2026-07-08 07:49:38 +0200

    https added fopr thew

--- CodeJournal.Api/Program.cs
+++ CodeJournal.Api/Program.cs
@@ -55,7 +55,7 @@ builder.Services.AddCors(options =>
         policy
             .WithOrigins(
                 "https://codejournalx.com",
-                "http://codejournalx.com",
+                "https://codejournalx.com",
                 "https://www.codejournalx.com",
                 "http://129.151.186.216:4200",
                 "http://localhost:4200",
